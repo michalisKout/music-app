a86d144f97f6ae9d8b11e4007f394206d459d3fe
Michalis Koutridis <michalis.koutridis@gmail.com>
2020-03-10 00:44:11 +0200

    docs: adds todos for real api scenario

--- src/app/containers/AlbumLinkContainer.js
+++ src/app/containers/AlbumLinkContainer.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import { DEFAULT_ALBUM } from '../utils/config';
 import AlbumLink from '../components/AlbumLink';
 
+//we should add a class container to handle api side effects
+
 const mapStateToProps = (state = {}, ownProps) => {
   const album = state.albums.entities[ownProps.albumId] || DEFAULT_ALBUM;
   return {
--- src/app/containers/AlbumsContainer.js
+++ src/app/containers/AlbumsContainer.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import Albums from '../components/Albums';
 
+//we should add a class container to handle api side effects
+
 const mapStateToProps = (state = {}) => {
   const albumIds = state.albums.ids || [];
   return {
