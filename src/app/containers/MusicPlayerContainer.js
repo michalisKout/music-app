a02d986d63eba97fe758df6d1f3c4052acf066bc
Michalis Koutridis <michalis.koutridis@gmail.com>
2020-03-10 00:45:25 +0200

    docs: adds todos for real api scenario

--- src/app/containers/MusicPlayerContainer.js
+++ src/app/containers/MusicPlayerContainer.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
 import MusicPlayer from '../components/MusicPlayer/MusicPlayer';
 
+//we should add a class container to handle api side effects
+
 const mapStateToProps = (state = {}) => {
   const trackId = state.playerConfig.trackId;
   const track = state.tracks.entities[trackId];
