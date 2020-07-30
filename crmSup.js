9de0acc987028cb8c7fdab1c7ddd43a0c81c0d75
Michael <50381321+michalisKout@users.noreply.github.com>
2020-07-30 14:44:51 +0300

    Update crmSup.js

--- crmSup.js
+++ crmSup.js
@@ -1,1 +1,33 @@
-console.log('hello from cdn!!!')
+const CRM_TOOLS = {
+  'lead': () => leadTool(),
+  'createWrite': () => createWrite(),
+}
+
+function leadTool() {
+   console.log('lead tooolings');  
+}
+
+function createWrite() {
+   console.log('creating writer');  
+}
+
+class orfiumSupportCRM {
+  static applyTool(toolName) {
+    const toolExistsInList = Object.keys(CRM_TOOLS).some(name => name === toolName);
+    const crmTool = CRM_TOOLS[toolName];
+    
+    if(!toolExistsInList) {
+      throw new Error('No tool exists with name: '+toolName);
+    }
+    
+    return crmTool();
+    
+  }
+}
+
+((windowObject) => {
+ windowObject && Object.defineProperty(windowObject, 'orfiumSupportCRM', {
+    value: orfiumSupportCRM,
+    writable: false
+  });
+}, window);
